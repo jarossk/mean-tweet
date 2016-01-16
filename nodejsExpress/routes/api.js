9f7b39bc900da90332e4c19550beec932e957c62
Jarek <klata.jarek@gmail.com>
2016-01-16 14:29:28 +0000

    Protecting APIs with authentication using Middleware

--- nodejsExpress/routes/api.js
+++ nodejsExpress/routes/api.js
@@ -1,6 +1,26 @@
 var express = require('express');
 var router = express.Router();
 
+// Used for routes that must be authenticated 
+function isAuthenticated(req, res, next) {
+  // if user is authenticated in the session, call the next() to call the next request handler
+  // Passport adds this method to request object. A middleware is allowed to add properties to
+  // request and response objects
+  
+  // allows all get request methods
+  if(req.method === "GET") {
+    return next();
+  }
+  if(isAuthenticated()) {
+    return next();
+  }
+  
+  // if the user is not authenticated then redirect hom to the login page
+  return res.redirect('#/login');
+};
+
+router.use('/posts', isAuthenticated);
+
 // api for all posts
 router.route('/posts')
 
