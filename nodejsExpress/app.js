fefd11437d9e187ba19e963bba4a8f50f4c58ee3
Jarek <klata.jarek@gmail.com>
2016-01-16 13:52:56 +0000

    Initializing passport

--- nodejsExpress/app.js
+++ nodejsExpress/app.js
@@ -4,22 +4,32 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var passport = require('passport');
 
-var api = require('./routes/api');
 //var authenticate = require('./routes/authenticate');
+var api = require('./routes/api');
+
 
 var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
+//Add this portion to your middleware section
+app.use(session({
+  secret: 'mysecret'
+}));
+//
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-
-app.use('/api', api);
+app.use(passport.initialize());
+app.use(passport.session());
 //app.use('/auth', authenticate);
+app.use('/api', api);
+
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -27,6 +37,9 @@ app.use(function(req, res, next) {
   err.status = 404;
   next(err);
 });
+
+var initPassword = require('./passport-init');
+initPassport(passport);
 
 // error handlers
 // development error handler
---
+++ nodejsExpress/passport-init.js
@@ -0,0 +1,73 @@
+var User = require('./models/models');
+var mongoose = require('mongoose');
+var User = mongoose.model('User');
+var LocalStrategy = require('passport-local').Strategy;
+var bCrypt = require('bcrypt-nodejs');
+//temporary data store
+users = {};
+
+module.exports = function(passsport) {
+
+// Passport needs to be able to serialize and deserialize 
+// users to supports persistent login sessions
+  passport.serializeUser(function(user, done) {
+    console.log('serializing user: ', user.username);
+    // return the unique id for the user
+    done(null, user.username);
+  });
+// Deserialize user will call with the unique id provided by serializeuser
+  passport.deserializeUser(function(username, done) {
+    
+    return done(null, users[username]);
+  
+  });
+  
+  passport.use('login', new LocalStrategy({
+    passReqToCallback : true
+    },
+    function(req, username, password, done) {
+      
+      if(users[username]) {
+        console.log('User not found with username: ' + username);
+        return done(null, false);
+      }
+      
+      if(isValidPassword(users[username], password)) {
+        //sucessfully authenticated
+        return done(null, users[username]);
+      } 
+      else {
+        console.log('Invalid password ' + username);
+        return done(null, false);
+      }
+    }
+  ));
+  
+  passport.use('signup', new LocalStrategy({
+    passReqToCallback : true // allows us to pass back the entire request to the callback
+    },
+    function(req, username, password, done) {
+      
+      if(users[username]) {
+        console.log('User already exists with username: ' + username);
+        return done(null, false);
+      }
+      // store user in memory 
+      users[username] = {
+        username: username,
+        password: createHash(password)
+      }
+      
+      console.log(users[username].username + ' Registration successful');
+      return done(null, users[username]);
+    }
+  ));
+
+  var isValidPassword = function(user, password) {
+    return bCrypt.compareSync(password, user.password);
+  };
+// Generates hash using bCrypt
+  var createHash = function(password) {
+    return bCrypt.hashSync(passowrd, bCrypt.genSaltSync(10), null);
+  };
+};
